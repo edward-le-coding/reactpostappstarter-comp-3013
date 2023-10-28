c72e99657309ee172675c4a3127d57356ed83dec
edward-le-coding <edward.le@alumni.ubc.ca>
2023-10-28 09:43:44 -0700

    Init commit

---
+++ frontend/src/ProviderLayout.jsx
@@ -0,0 +1,30 @@
+import {
+  DEFAULT_THEME,
+  mergeMantineTheme,
+  MantineProvider,
+  createTheme,
+  rem,
+} from "@mantine/core";
+const themeOverride = createTheme({
+  fontFamily: "Verdana, sans-serif",
+  white: "#FAFAFA",
+  spacing: {
+    xs: rem(4),
+    sm: rem(8),
+    md: rem(16),
+    lg: rem(24),
+    xl: rem(32),
+  },
+  titleFontSize: rem(32),
+  cardHeight: rem(440),
+});
+const theme = mergeMantineTheme(DEFAULT_THEME, themeOverride);
+export default ({ children }) => {
+  return (
+    <>
+      <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+        {children}
+      </MantineProvider>
+    </>
+  );
+};
