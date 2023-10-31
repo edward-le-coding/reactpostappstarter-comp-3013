5d06115c4aa50e7a1ad3f30c326da7f42b094c90
edward-le-coding <edward.le@alumni.ubc.ca>
2023-10-31 09:24:47 -0700

    Added light/dark mode button

--- frontend/src/ProviderLayout.jsx
+++ frontend/src/ProviderLayout.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import ColorSchemeContext from './ColorSchemeContext';
 import {
   DEFAULT_THEME,
   mergeMantineTheme,
@@ -6,26 +8,29 @@ import {
   rem,
 } from "@mantine/core";
 import DrawerProvider from "./Contexts/drawerContext";
-const themeOverride = createTheme({
-  fontFamily: "Verdana, sans-serif",
-  white: "#FAFAFA",
-  spacing: {
-    xs: rem(4),
-    sm: rem(8),
-    md: rem(16),
-    lg: rem(24),
-    xl: rem(32),
-  },
-  titleFontSize: rem(32),
-  cardHeight: rem(440),
-});
-const theme = mergeMantineTheme(DEFAULT_THEME, themeOverride);
 export default ({ children }) => {
+  const [colorScheme, setColorScheme] = React.useState('dark');
+  const themeOverride = createTheme({
+    fontFamily: "Verdana, sans-serif",
+    white: "#FAFAFA",
+    spacing: {
+      xs: rem(4),
+      sm: rem(8),
+      md: rem(16),
+      lg: rem(24),
+      xl: rem(32),
+    },
+    titleFontSize: rem(32),
+    cardHeight: rem(440),
+    colorScheme: colorScheme,
+  });
   return (
     <>
-      <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-        <DrawerProvider>{children}</DrawerProvider>
-      </MantineProvider>
+      <ColorSchemeContext.Provider value={{ colorScheme, onChange: setColorScheme }}>
+        <MantineProvider theme={mergeMantineTheme(DEFAULT_THEME, themeOverride)} withGlobalStyles withNormalizeCSS>
+          <DrawerProvider>{children}</DrawerProvider>
+        </MantineProvider>
+      </ColorSchemeContext.Provider>
     </>
   );
 };
--- frontend/src/components/misc/Navbar.jsx
+++ frontend/src/components/misc/Navbar.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import classes from "./Navbar.module.css";
 import { MantineLogo } from "@mantine/ds";
-import { Container, Group, Burger, Drawer, Stack } from "@mantine/core";
+import { ActionIcon, Container, Group, Burger, Drawer, Stack } from "@mantine/core";
 import useLinks from "./useLinks";
 import { DrawerContext } from "../../Contexts/drawerContext";
+import { SunIcon, MoonIcon } from '@modulz/radix-icons';
+import ColorSchemeContext from '../../ColorSchemeContext';
 
 const Navbar = () => {
   const { opened, toggle } = React.useContext(DrawerContext);
   const [items] = useLinks();
+  const colorSchemeContext = React.useContext(ColorSchemeContext);
+  const dark = colorSchemeContext.colorScheme === 'dark';
 
   return (
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
         <MantineLogo size={28} />
+        {toggleDarkModeButton()}
         <Group gap={5} visibleFrom="xs">
           {items}
         </Group>
@@ -28,6 +33,21 @@ const Navbar = () => {
       </Container>
     </header>
   );
+
+  function toggleDarkModeButton() {
+    return <ActionIcon
+      variant="outline"
+      color={dark ? 'yellow' : 'blue'}
+      onClick={() => colorSchemeContext.onChange(dark ? 'light' : 'dark')}
+      title="Toggle color scheme"
+    >
+      {dark ? (
+        <SunIcon style={{ width: 18, height: 18 }} />
+      ) : (
+        <MoonIcon style={{ width: 18, height: 18 }} />
+      )}
+    </ActionIcon>;
+  }
 };
 
 export default Navbar;
