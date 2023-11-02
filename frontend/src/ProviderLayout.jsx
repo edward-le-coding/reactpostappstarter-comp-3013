fa14e395b2ee255fa62ba03149443e5b4dcbb026
edward-le-coding <edward.le@alumni.ubc.ca>
2023-11-02 00:09:38 -0700

    Saved dark mode to local storage

--- frontend/src/ProviderLayout.jsx
+++ frontend/src/ProviderLayout.jsx
@@ -8,8 +8,19 @@ import {
   rem,
 } from "@mantine/core";
 import DrawerProvider from "./Contexts/drawerContext";
+import { useWindowEvent, useLocalStorage } from '@mantine/hooks';
 export default ({ children }) => {
-  const [colorScheme, setColorScheme] = React.useState('light');
+  const [colorScheme, setColorScheme] =  useLocalStorage({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+  });
+
+  useWindowEvent('keydown', (event) => {
+    if (event.code === 'KeyJ' && (event.ctrlKey || event.metaKey)) {
+      setColorScheme((current) => (current === 'dark' ? 'light' : 'dark'));
+    }
+  });
+
   const themeOverride = createTheme({
     fontFamily: "Verdana, sans-serif",
     white: "#FAFAFA",
